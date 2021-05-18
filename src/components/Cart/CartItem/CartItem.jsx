a78e4bdd7a7e7c24add29773f3897bafbe372f90
tkachuks <serhiytkachuk88@gmail.com>
2021-05-19 00:13:52 +0300

    upd responsive styles

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -3,23 +3,26 @@ import s from './CartItem.module.css';
 const CartItem = ({ i, onRemoveFromCart, onIncrementCartItem, onDecrementCartItem }) => {
   return (
     <li className={s.Item}>
-      <button type='button' className={s.Btn} onClick={() => onRemoveFromCart(i.id)}><img src={config.urls.media + 'close.svg'} alt='' /></button>
-      <div className={s.Price_wrap}>
-        <span className={s.Discount}>{
-          i.discount_price <= i.price
-            ? i.discount_price == 0 ? config.translations.free_label : i.discount_price + config.currency
-            : i.price + config.currency}
-        </span>
+      <div className={s.Cont}>
+        <button type='button' className={s.Btn} onClick={() => onRemoveFromCart(i.id)}><img src={config.urls.media + 'close.svg'} alt='' /></button>
+        <div className={s.Price_wrap}>
+          <span className={s.Discount}>{
+            i.discount_price <= i.price
+              ? i.discount_price == 0 ? config.translations.free_label : i.discount_price + config.currency
+              : i.price + config.currency}
+          </span>
+        </div>
+        <div className={s.Controls}>
+          <button type='button' className={s.Btn} onClick={() => onIncrementCartItem(i.id)}>
+            <img src={config.urls.media + 'plus.svg'} alt='' />
+          </button>
+          <span className={s.Qty}>{i.amount}</span>
+          <button type='button' className={s.Btn} onClick={() => onDecrementCartItem(i.id)}>
+            <img src={config.urls.media + 'minus.svg'} alt='' />
+          </button>
+        </div>
       </div>
-      <div className={s.Controls}>
-        <button type='button' className={s.Btn} onClick={() => onIncrementCartItem(i.id)}>
-          <img src={config.urls.media + 'plus.svg'} alt='' />
-        </button>
-        <span className={s.Qty}>{i.amount}</span>
-        <button type='button' className={s.Btn} onClick={() => onDecrementCartItem(i.id)}>
-          <img src={config.urls.media + 'minus.svg'} alt='' />
-        </button>
-      </div>
+
       <figure className={s.Pic}>
         <figcaption>{i.name}</figcaption>
         <picture>
