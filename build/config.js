424061ce703fe10dc55e3c3ed06e5d5991a3d5c8
doc999tor <doc999tor@gmail.com>
2021-09-04 04:14:36 +0300

    added-ferns+rotala-indica

--- build/config.js
+++ build/config.js
@@ -11,7 +11,7 @@ const config = {
             id: 'myriophyllum_aquaticum',
             name: 'Myriophyllum mattogrossense - מיריופילום מטוגרוסנס',
             photo: ['myriophyllum_aquaticum_1'],
-            description: 'צמח מהיר-גדילה מקסים, עם צבע ירוק-רענן, שגודל בתנאים טובים ממש מהר ויוצר סבך יפה\nמנה אחת - 3 גבועלים בוגרים',
+            description: 'צמח מהיר-גדילה מקסים, עם צבע ירוק-רענן, שגודל בתנאים טובים ממש מהר ויוצר סבך יפה\nמנה אחת - 3 גבעולים בוגרים',
             price: 35,
             discount_price: 35,
             qty_stock: 15 // 0 is not in stock
@@ -20,7 +20,7 @@ const config = {
             id: 'cabomba_carolineana',
             name: 'Cabomba caroliniana - קבומבה קרוליניאנה',
             photo: ['cabomba_carolineana_2', 'cabomba_carolineana_3'],
-            description: 'צמח גדול, מהיר-גדילה, עם עלים בצבע ירוק-כהה וסגלגל בצד התחתון\nמעולה ללאו-טק, לא מחייב דישון, CO2 או תאורה מעל 0.7 ווט לליטר. בתאורה חזקה יותר מקבל צבע אדום יפה\nמנה אחת - 3 גבועלים בוגרים',
+            description: 'צמח גדול, מהיר-גדילה, עם עלים בצבע ירוק-כהה וסגלגל בצד התחתון\nמעולה ללאו-טק, לא מחייב דישון, CO2 או תאורה מעל 0.7 ווט לליטר. בתאורה חזקה יותר מקבל צבע אדום יפה\nמנה אחת - 3 גבעולים בוגרים',
             price: 45,
             discount_price: 45,
             qty_stock: 1 // 0 is not in stock
@@ -29,7 +29,7 @@ const config = {
             id: 'vallisneria-spiralis',
             name: 'Vallisneria spiralis - וולסנריה מסולסלת',
             photo: ['vallisneria-spiralis'],
-            description: 'צמח גבוה אם גדל בצפיפות, מהיר-גדילה, מתפשט מהר לצדדים ויוצר ג\'ונגל מאוד מרשים\nמעולה ללאו-טק, לא מחייב דישון, CO2 או תאורה חזקה\nמנה אחת - 3 גבועלים בוגרים',
+            description: 'צמח גבוה אם גדל בצפיפות, מהיר-גדילה, מתפשט מהר לצדדים ויוצר ג\'ונגל מאוד מרשים\nמעולה ללאו-טק, לא מחייב דישון, CO2 או תאורה חזקה\nמנה אחת - 3 גבעולים בוגרים',
             price: 35,
             discount_price: 15,
             qty_stock: 10 // 0 is not in stock
@@ -47,7 +47,7 @@ const config = {
             id: 'sagittaria_subulata',
             name: 'Sagittaria subulata - סגיטריה סובולטה',
             photo: ['sagittaria_subulata_1', 'sagittaria_subulata_2'],
-            description: 'צמח דשא או לקידמה של אקווריומי לאו-טק\nמנה אחת - 3 גבועלים בוגרים',
+            description: 'צמח דשא או לקידמה של אקווריומי לאו-טק\nמנה אחת - 3 גבעולים בוגרים',
             price: 35,
             discount_price: 35,
             qty_stock: 5 // 0 is not in stock
@@ -79,6 +79,15 @@ const config = {
             discount_price: 45,
             qty_stock: 10 // 0 is not in stock
           },
+          {
+            id: 'rotala_rotundifolia',
+            name: 'Rotala indica rotundifolia - רוטלה אינדיקה רוטנדיפוליה / רוטונדיפוליה',
+            photo: ['rotala_rotundifolia_1', 'rotala_rotundifolia_2'],
+            description: 'רוטלה אינדיקה זה צמח גבעול אהוב מאוד על ידי מתחילים, לא דורש תנאים מיוחדים, גודל יפה מאוד בכל אקווריום\nאבל על מנת להפיק ממנו מקסימום - בתנאי מיד-טק לא תזהו את הרוטלה, זה יהיה צמח אחר למרי: אדום, עם עלים חזקים, שיוצר סבך בפן האחורי\nמנה אחת - 3 גבעולים בוגרים',
+            price: 35,
+            discount_price: 35,
+            qty_stock: 3 // 0 is not in stock
+          },
         ]
       },
       {
@@ -98,7 +107,7 @@ const config = {
             id: 'myriophyllum_heterophyllum',
             name: 'Myriophyllum heterophyllum - מיריופילום עם גבעול אדום',
             photo: ['myriophyllum_heterophyllum_1', 'myriophyllum_heterophyllum_2', ],
-            description: 'מיריופילום יפה מאוד, עם גבעול אדום ועלים ירוקים\nמנה אחת - 3 גבועלים בוגרים',
+            description: 'מיריופילום יפה מאוד, עם גבעול אדום ועלים ירוקים\nמנה אחת - 3 גבעולים בוגרים',
             price: 45,
             discount_price: 35,
             qty_stock: 5 // 0 is not in stock
@@ -107,7 +116,7 @@ const config = {
             id: 'didiplis-diandra',
             name: 'Didiplis diandra - דידיפליס דיאנדרה',
             photo: ['didiplis-diandra-1', 'didiplis-diandra-2'],
-            description: 'צמח מהיר-גדילה מקסים\nמתאים לעיצוב של אחורית וצדדים של האק\'\nדורש תאורה טובה\nמנה אחת - 3 גבועלים בוגרים',
+            description: 'צמח מהיר-גדילה מקסים\nמתאים לעיצוב של אחורית וצדדים של האק\'\nדורש תאורה טובה\nמנה אחת - 3 גבעולים בוגרים',
             price: 45,
             discount_price: 35,
             qty_stock: 3 // 0 is not in stock
@@ -116,7 +125,7 @@ const config = {
             id: 'echinodorus-latifolius',
             name: 'Echinodorus latifolius - אכינודורוס לטיפוליוס',
             photo: ['echinodorus-latifolius-1', 'echinodorus-latifolius-2'],
-            description: 'צמח לקידמת האק\' (רמה מתקדמת - היי-טק)\nמנה אחת - 3 גבועלים בוגרים',
+            description: 'צמח לקידמת האק\' (רמה מתקדמת - היי-טק)\nמנה אחת - 3 גבעולים בוגרים',
             price: 10,
             discount_price: 10,
             qty_stock: 30 // 0 is not in stock
@@ -125,10 +134,52 @@ const config = {
             id: 'hygrophila_tiger',
             name: 'היגרופליה טייגר - Hygrophila sp. Tiger',
             photo: ['hygrophila_tiger_1', 'hygrophila_tiger_2', 'hygrophila_tiger_3'],
-            description: 'צמח היי-טק נדיר ומיוחד - Hygrophila sp. Tiger רק למביני עניין מהיר-גדילה, מעולה לאחורנית\nמנה אחת - 3 גבועלים בוגרים',
+            description: 'צמח היי-טק נדיר ומיוחד - Hygrophila sp. Tiger רק למביני עניין מהיר-גדילה, מעולה לאחורנית\nמנה אחת - 3 גבעולים בוגרים',
             price: 50,
             discount_price: 35,
             qty_stock: 2 // 0 is not in stock
+          },
+        ]
+      },
+      {
+        title: 'מוסים (טחבים) ושרכים',
+        sub_title: 'מוסים ושרכים הם אבני בניין של כל אקווריום צמחייה או אקווה סקייפ. רובם לא דורשים תנאים מיוחדים, יכולים לגדול בכל צנצנצ מים נקיים. מתשמשים בהם הרבה כדי לעצב תוואי שטח, להסתיר ציוד, בתור מצע לשרימסים והטלת דגי חתול',
+        plants: [
+          {
+            id: 'microsorum_pteropus_windelov',
+            name: 'שרך תאילנדי (מיקרוסוריום פטרופוס) ווינדלוב - Microsorum pteropus Windelov, Windelov java fern',
+            photo: ['microsorum_pteropus_windelov'],
+            description: '',
+            price: 35,
+            discount_price: 35,
+            qty_stock: 3 // 0 is not in stock
+          },
+          {
+            id: 'microsorum_pteropus_narrow_leaf',
+            name: 'שרך תאילנדי (מיקרוסוריום פטרופוס) - Microsorum pteropus \'Narrow Leaf\'',
+            photo: ['microsorum_pteropus_narrow_leaf'],
+            description: '',
+            price: 35,
+            discount_price: 35,
+            qty_stock: 5 // 0 is not in stock
+          },
+          {
+            id: 'microsorum_pteropus_trident',
+            name: 'שרך תאילנדי (מיקרוסוריום פטרופוס) טריידנט - Microsorum pteropus Trident, Trident java fern',
+            photo: ['microsorum_pteropus_trident'],
+            description: '',
+            price: 35,
+            discount_price: 35,
+            qty_stock: 5 // 0 is not in stock
+          },
+          {
+            id: 'bolbitis_heudelotii',
+            name: 'בולביטיס הודלוטי - Bolbitis heudelotii (African water fern, Congo fern)',
+            photo: ['bolbitis_heudelotii_3', 'bolbitis_heudelotii_2', 'bolbitis_heudelotii_1', ],
+            description: '',
+            price: 50,
+            discount_price: 50,
+            qty_stock: 3 // 0 is not in stock
           },
         ]
       },
